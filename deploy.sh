88cbb19be394c012e01d29fde6276cf3e6e14608
Nikolai <n.dunkel@gmail.com>
2018-08-04 21:59:19 -0700

    newest blog code deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 #hugo # if using a theme, replace with 
 hugo -t hyde
 
+git add .
+git commit -m "newest blog code deploy"
+git push origin master
+
 # Go To Public folder
 cd public
 # Add changes to git.
