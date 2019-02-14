c2e73251c9a6ba16bb0fd2724b77ae4cd4509ea8
Nikolai <n.dunkel@gmail.com>
2019-02-14 11:18:21 -0800

    newest blog source deploy Thu Feb 14 11:18:21 PST 2019

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
+echo -e "\033[0;32mBuilding Site...\033[0m"
+
+hugo
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 git add .
 git commit -m "newest blog source deploy `date`"
 git push origin master
 
-echo -e "\033[0;32mDeploying updates to GitHub Pages...\033[0m"
-# Build the project.
-#hugo # if using a theme, replace with 
-hugo
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master -f
-
-# Come Back up to the Project Root
-cd ..
